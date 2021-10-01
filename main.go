e8fd5b8230ac98c5ce2129e6c916b9cb3b16c9d2
winartodev <winarto007zing@gmail.com>
2021-10-01 07:45:57 +0700

    adding array configuration and remove unused variabels

--- main.go
+++ main.go
@@ -14,28 +14,30 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var (
-	username = os.Getenv("DB_USERNAME")
-	password = os.Getenv("DB_PASSWORD")
-	address  = os.Getenv("DB_ADDRESS")
-	port     = os.Getenv("DB_PORT")
-	dbname   = os.Getenv("DB_DBNAME")
-)
-
 func main() {
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, address, port, dbname))
+	// Mysql Configuration
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_ADDRESS"), os.Getenv("DB_PORT"), os.Getenv("DB_DBNAME")))
+
 	if err != nil {
 		panic(err.Error())
 	}
+
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	pokemonInterface := models.Connect(db)
 	pokemonController := controllers.NewPokemonController(pokemonInterface)
 	view.EndpointsHandler(pokemonController)
 
-	addr := flag.String("addr", ":8080", "Http Listen And Serve")
+	// Array Configuration
+	// var pokemonStruct = &pokemon.Entity{}
+	// pokemonInterface := models.WithStruct(pokemonStruct)
+	// pokemonController := controllers.NewPokemonController(pokemonInterface)
+	// view.EndpointsHandler(pokemonController)
+
+	log.Println("started at : http://127.0.0.1:8080/")
+	var addr = flag.String("addr", ":8080", "Http Listen And Serve")
 	e := http.ListenAndServe(*addr, nil)
-	if e == nil {
-		log.Fatal("ListenAndServe:", err)
+	if e != nil {
+		log.Fatal("ListenAndServe:", e)
 	}
-	fmt.Println("ListenAndServe:8080")
 }
